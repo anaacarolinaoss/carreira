0b4ab1f2a38e295e6b98e3b6173d30eede2a612a
ana carolina <carolinaanaa191@gmail.com>
2023-08-24 11:34:50 -0300

    Aula do dia 24 de agosto.

---
+++ router.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/RouteSwitch.php';
+
+class Router extends RouterSwitch
+{
+  /**
+   * summary of run
+   * @param string $requestUri
+   * @return void
+    */  
+
+public function run(string $requestUri)
+{
+    $route = substr($requestUri, 1);
+    $route = str_replace("carreira/","",$route);
+
+ if($route == ''){
+    $this->home();
+    
+ } else{
+    $this->$route();
+ }
+
+}
+
+
+}
